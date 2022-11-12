c01c5dc03dd1e9f5707c3ea1562c71d2187b5de5
Matthijs Stoffer <63472519+matthijs12310@users.noreply.github.com>
2022-11-12 17:44:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+var http = require("http");
+
+http.createServer(function (request, response) {
+    // Send the HTTP header 
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    const { spawn } = require('child_process')
+
+// start the `ping google.com` command
+const command = spawn('lscpu')
+
+// the `data` event is fired every time data is
+// output from the command
+command.stdout.on('data', output => {
+    // the output data is captured and printed in the callback
+    console.log("Output: ", output.toString())
 })
-app.listen(process.env.PORT || 3000)
+    // Send the response body as "Hello World"
+    response.end('Hello World\n');
+ }).listen(8081);
+ 
+ // Console will print the message
+ console.log('Server running at http://127.0.0.1:8081/');
